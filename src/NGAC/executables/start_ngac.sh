3dce31df0cd074d04ebcd80a6c926a331028ec36
ivario123 <ivar.jonsson18@gmail.com>
2022-12-02 15:31:07 +0100

    run the correct files

--- src/NGAC/executables/start_ngac.sh
+++ src/NGAC/executables/start_ngac.sh
@@ -6,25 +6,21 @@ if [ -f /etc/debian_version ]; then
     # We are running on Debian
     echo "Starting NGAC server on Debian"
     ./linux/ngac_server &
-    ./linux/cme_sim &
-    ./linux/PEP-RAP/pep_server &
-elif [ -f /etc/arch-release ]; then
+    ./linux/cme &
+    ./linux/pep_server &
+    elif [ -f /etc/arch-release ]; then
     # We are running on Arch
     echo "Starting NGAC server on Arch"
     ./arch/ngac_server &
-    ./arch/cme_sim &
-    ./arch/PEP-RAP/pep_server &
-elif [ -f /etc/os-release ]; then
+    ./arch/cme &
+    ./arch/pep_server &
+    elif [ -f /etc/os-release ]; then
     # We are running on Mac
     echo "Starting NGAC server on Mac"
     ./mac/ngac_server &
-    ./mac/cme_sim &
-    ./mac/PEP-RAP/pep_server &
+    ./mac/cme &
+    ./mac/pep_server &
 else
     # Unknown.
     echo "Unknown OS"
 fi
-
-# Start the CME server
-
-# Start the NGAC server
