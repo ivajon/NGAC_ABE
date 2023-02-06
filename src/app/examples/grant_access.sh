322322f1c2487f00f6180a3409e24f687e366b53
ivario123 <ivar.jonsson18@gmail.com>
2023-02-06 16:38:20 +0100

    add demo examples

---
+++ src/app/examples/grant_access.sh
@@ -0,0 +1,23 @@
+# Load ipolicy.pl so we know where we are starting
+python src admin -t admin_token loadi -i ipolicy.pl
+
+# User 1 wants to make a file
+python src -u u1  create --file o1 -oa o1,o2
+# Write to that file
+python src -u u1  write --file o1 -i pyproject.toml
+# Read the file
+python src -u u1 -a ua1 read --file o1
+
+# User 2 wants to read the same file
+python src -u u2 -a ua1 read --file o1
+echo "This should have failed"
+
+# User 2 tells admin to grant access
+python src admin -t admin_token assign -u u2 -a ua1
+python src -u u2 -a ua1 read --file o1
+
+
+
+
+# Delete the file
+python src -u u1 -a ua1 delete --file o1
---
+++ src/app/examples/write_file.sh
@@ -0,0 +1,1 @@
+python src -u u1  write --file o1 -i pyproject.toml
